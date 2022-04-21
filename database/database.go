b7c9af998ee5e168e7851c4fb8f24d1b7265c63d
sarishaa <sarishaprajapati@bajratechnologies.com>
2022-04-21 16:54:51 +0545

    made some changes in database code

--- database/database.go
+++ database/database.go
@@ -9,33 +9,46 @@ import (
 )
 
 type User struct {
-	Userid   int
+	ID       int
 	Username string
-	Email    string
+	Email    string `gorm:"typevarchar(100)"`
 	Password string
 }
 
 type UserDetails struct {
-	UserDid int
+	ID      int
 	Name    string
 	Address string
-	Phone   string
+	Phone   string `gorm:"unique_index"`
 	UserID  int
 }
 
+// var Db *gorm.DB
+
+// func Init() *gorm.DB {
+// 	dbURL := "postgres://sarisha:sarisha.123@localhost:5432/goauthentication?sslmode=disable"
+// 	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+// 	if err != nil {
+// 		log.Panic(err)
+// 	} else {
+// 		fmt.Println("Connected to database")
+// 	}
+// 	Db = db
+
+// 	// Migration to create tables for user and userdetail schema
+// 	db.AutoMigrate(&model.User{}, &model.UserDetail{})
+// 	return db
+// }
 var Db *sql.DB
 
 func Init() *sql.DB {
 	dbURL := "postgres://sarisha:sarisha.123@localhost:5432/goauthentication?sslmode=disable"
-
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
 		log.Panic(err)
 	} else {
 		fmt.Println("Connected to database")
 	}
-
 	Db = db
 	return db
-
 }
